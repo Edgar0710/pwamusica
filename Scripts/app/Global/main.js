038816558f55c05de7e6578cfa55f0958b581740
Edgar Carrera <edgar.carrera@utxicotepec.edu.mx>
2021-11-06 19:06:08 -0600

    update pwa

--- Scripts/app/Global/main.js
+++ Scripts/app/Global/main.js
@@ -1,14 +1,46 @@
+let newServiceWorker;
 var Main={
 Init: function() {
 Main.addSw();
+Main.addEvents();
+},
+addEvents:function(){
+
+document.getElementById('btn-update').addEventListener('click',()=>{
+newServiceWorker.postMessage({action:'skipWaiting'});
+window.location.reload();
+});
+
 },
 addSw: function(){
-    navigator.serviceWorker.register("/sw.js").then((r)=>{
-        console.log("data then =>"+r);
-    }).catch((err)=>{
+    navigator.serviceWorker.register("/sw.js").then((registration)=>{
+        registration.addEventListener('updatefound',()=>{
+            newServiceWorker=registration.installing;
+            newServiceWorker.addEventListener('statechange', ()=>{
+                //if , switch
+                switch(newServiceWorker.state){
+                    case 'installed':
+                        //lanzar snackbar
+                        Main.ShowSanckBar();
+                        break;
+                        default:
+                            console.log('Unknown');
+                        break;
+                }
+            })
+    })
+}).catch((err)=>{
         console.log("data error =>"+err);
     })
 
+},
+ShowSanckBar: function(){
+    var  x= document.getElementById('snackbar');
+    x.className='show';
+},
+HideSanckBar: function(){
+    var  x= document.getElementById('snackbar');
+    x.className = x.className.replace("show", "");
 }
 }
  window.addEventListener("load", function() {
--- sw.js
+++ sw.js
@@ -1,53 +1,151 @@
-self.addEventListener('install', function(event) {
-  let nameCache = "appCache";
-let files=[
-'/index.html',
-'/Content/css/vendors/bootstrap.css',
-'/Content/Images/avatar.jpg',
-'/Content/Images/not-connection.jpg'
-];
-caches.open(nameCache).then(cache =>{
-return cache.addAll(files);
-}).catch(error =>{
+
+const _staticCache='staticCache@v2';//Save local data
+const _dynamicCache='dynamicCache@v1.2';//Save files that not contempled for the user
+const _inmutableCahe='inmutableCahe@v1';//Save data that  never change
+self.addEventListener('install', function (eventinstall) {
+
+  let files_appshell = [
+    "/",
+    '/index.html',
+    '/Content/css/vendors/bootstrap.css',
+    '/Content/Images/avatar.jpg',
+    '/Content/Images/not-connection.jpg',
+    '/Content/Images/not-founf.png',
+    '/Scripts/app/cantante.js',
+    '/Content/css/app/style.css'
+   
+  ];
+  let files_inmutables = [
+ 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'
+  ];
+  const static_cache=caches.open(_dynamicCache).then(cache => {
+    return cache.addAll(files_appshell);
+  }).catch(error => {
     console.log(error);
+  });
+  const static_inmutable=caches.open(_inmutableCahe).then(cache => {
+    return cache.addAll(files_inmutables);
+  }).catch(error => {
+    console.log(error);
+  });
+eventinstall.waitUntil(
+Promise.allSettled([static_cache,static_inmutable])
+);
+});
+
+if (!(navigator.onLine)) {
+  goOnline();
+}
+
+self.addEventListener('online', function () {
+  goOnline();
+});
+
+self.addEventListener('offline', function () {
+  goOnline();
+});
+
+self.addEventListener('activate', event=>{
+
+  event.waitUntil(
+      caches.keys().then(cachesList=>Promise.all(
+          cachesList.map(cacheName=>{
+              if(_staticCache!=(cacheName)&&cacheName!=_inmutableCahe){
+                  return caches.delete(cacheName);
+              }
+          })
+      )).then(()=>{
+          console.log('eleminados')
+      })
+  )
+ 
 })
 
-});
 
-if(!(navigator.onLine)) {
-  goOnline();
-  }
 
-self.addEventListener('online', function(){
-  goOnline();
+
+self.addEventListener('fetch', (event) => {
+  /*1.Catch Only: La aplicación solamente responde lo que se enecuebta en cache*/
+/// event.respondWith(caches.match(event.request));
+ 
+
+  //2.Network Only=>Responde con datos desde internet solamente 
+  
+ ///  event.respondWith(fetch(event.request));
+ 
+
+  //3. Cache first: Primero se va a buscar las peticiones al cache, y en caso de que no este 
+  //va a la red
+  // const res = caches.match(event.request)
+  // .then((param) => {
+  //   return param ? param : fetch(event.request);
+  // })
+  // .catch((error) => {
+  //   console.log(error); 
+  // });
+
+///Network First: Busca primero en internet y si no lo encuentra busca en cache.
+// const res = fetch(event.request)
+//  .then((param) => {
+//      return param ? param : caches.match(event.request); 
+//   })
+// .catch((error) => {
+//    console.log(error);
+//   });
+//5 Network First with fallback  
+// const res = fetch(event.request)
+//   .then((param) => {
+//       return param ? param : caches.match(event.request).then((cacheResponse) => {
+
+//       }); 
+//    })
+//  .catch((error) => {
+//     console.log(error);
+//    });
+//6.0 Cache First with FallBack
+const res = caches.match(event.request)
+.then((param) => {
+  return param ? param : fetch(event.request).then(fetchResponse=>{
+    caches.open(_dynamicCache).then(cache=>{
+      cache.put(event.request,fetchResponse);
+       return fetchResponse.clone();
+    })
   });
-  
-  self.addEventListener('offline',function(){
-  goOnline();
-  });
-  
-
-  
-
-
-
-self.addEventListener('fetch', function(event) {
-event.respondWith(caches.match(event.request).then((param)=> {
-if(param)return param;
-return fetch(event.request);
-}));
+})
+.catch((error) => {
+  console.log(error); 
 });
-
-
-
+event.respondWith(res);
+});
 function goOnline() {
   self.location.reload();
 }
 function goOffline() {
-  document.getElementById("list_song").innerHTML=`   <div class="col text-center" style="padding: 90px 0px;">
+  document.getElementById("list_song").innerHTML = `   <div class="col text-center" style="padding: 90px 0px;">
   <div style="font-size: 100px;">
      <img src="Content/Images/not-connection.png" class="img-responsive" alt="not-foud">
   </div>
       <div class=""><h4 class="card-title mb-1">Ups... Lo sentimos no tienes conexión a internet.</h4></div>
    </div>`;
+}
+
+self.addEventListener('message',object=>{
+  switch(object.data.action){
+    case 'skipWaiting':
+    self.skipWaiting();
+    break;
+    case '':
+      break;
+    default:
+      console.log("Erro")
+      break;
+  }
+})
+
+
+
+function handleError() {
+  console.log('error');
 }

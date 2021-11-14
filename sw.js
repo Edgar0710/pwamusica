7ea567826da53d0061af075bd42ca9de052e3fb8
Edgar Carrera <edgar.carrera@utxicotepec.edu.mx>
2021-11-14 17:43:36 -0600

    limpieza

--- sw.js
+++ sw.js
@@ -11,25 +11,15 @@ self.addEventListener('install', function (eventinstall) {
     '/Content/Images/avatar.jpg',
     '/Content/Images/not-connection.jpg',
     '/Content/Images/not-founf.png',
-    '/Scripts/app/cantante.js',
     '/Content/css/app/style.css'
-   
   ];
-  let files_inmutables = [
- 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js',
-    'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'
-  ];
+
   const static_cache=caches.open(_staticCache).then(cache => {
     return cache.addAll(files_appshell);
   }).catch(error => {
     console.log(error);
   });
-  // const static_inmutable=caches.open(_inmutableCahe).then(cache => {
-  //   return cache.addAll(files_inmutables);
-  // }).catch(error => {
-  //   console.log(error);
-  // });static_inmutable
+ 
 eventinstall.waitUntil(
 Promise.allSettled([static_cache])
 );
@@ -68,18 +58,7 @@ self.addEventListener('activate', event=>{
 
 self.addEventListener('fetch', (event) => {
 
-// const res = caches.match(event.request)
-// .then((param) => {
-//   return param ? param : fetch(event.request).then(fetchResponse=>{
-//     caches.open(_dynamicCache).then(cache=>{
-//       cache.put(event.request,fetchResponse.clone());
-//        return fetchResponse;
-//     })
-//   });
-// })
-// .catch((error) => {
-//   console.log(error); 
-// });
+
 const res = caches.match(event.request)
  .then((param) => {
    return param ? param : fetch(event.request);
